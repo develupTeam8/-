5e4e42f3cf22000fc27c1dd430c010aa4b420a28
soromi <soromii@naver.com>
2019-12-01 06:10:35 +0900

    Updates

---
+++ precache-manifest.09b8916558be25f9f589aded1f8fbc10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd6b9d839321dbfcfdf76155365d81c",
+    "url": "/junggonyangi-/index.html"
+  },
+  {
+    "revision": "6acf187b6b62e0008ea6",
+    "url": "/junggonyangi-/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "299e56f57919743e7144",
+    "url": "/junggonyangi-/static/css/main.21f9156f.chunk.css"
+  },
+  {
+    "revision": "6acf187b6b62e0008ea6",
+    "url": "/junggonyangi-/static/js/2.1ab19e0b.chunk.js"
+  },
+  {
+    "revision": "299e56f57919743e7144",
+    "url": "/junggonyangi-/static/js/main.92d4d7d5.chunk.js"
+  },
+  {
+    "revision": "d612e21397a1f61cfacb",
+    "url": "/junggonyangi-/static/js/runtime-main.68d95038.js"
+  },
+  {
+    "revision": "bffda1d8754a6bec49de7113b8bd0e23",
+    "url": "/junggonyangi-/static/media/main.bffda1d8.jpg"
+  },
+  {
+    "revision": "241d5992244d15f049c731ec2a7eda1d",
+    "url": "/junggonyangi-/static/media/product.241d5992.jpg"
+  }
+]);
